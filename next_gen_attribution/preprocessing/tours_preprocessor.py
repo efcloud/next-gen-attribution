23a92557340cbe8b3c1450aaa29ee623d99c9c2c
Hans Bantilan <hansbantilan@gmail.com>
2022-11-18 07:55:56 +0000

    put all of adobe into email as per Vivek's 2022-11-17-1757 message

--- next_gen_attribution/preprocessing/tours_preprocessor.py
+++ next_gen_attribution/preprocessing/tours_preprocessor.py
@@ -4,6 +4,11 @@ data = pd.read_csv("file_for_hans_2.csv")
 
 data.rename(columns = {list(data)[0]:'user_id'}, inplace=True)
 
+# put all of adobe into email as per Vivek's 2022-11-17-1757 message "source=email and source=adobe is one and the same"
+data["utm_source_email"] = data["utm_source_email"] + data["utm_source_adobe"]
+data = data.drop(columns=["utm_source_adobe"])
+
+# create journey vector
 non_journey_columns = ["user_id", "_uid", "is_converted"]
 utm_source_columns = list(data.filter(regex="utm_source_"))
 utm_campaign_columns = list(data.filter(regex="utm_campaign_"))
