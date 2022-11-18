7ba1abf94b992ce87d0fe3b2fea9ae90b45a1078
Hans Bantilan <hansbantilan@gmail.com>
2022-11-18 07:49:38 +0000

    add tours preprocessor

---
+++ next_gen_attribution/preprocessing/tours_preprocessor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = pd.read_csv("file_for_hans_2.csv")
+
+data.rename(columns = {list(data)[0]:'user_id'}, inplace=True)
+
+non_journey_columns = ["user_id", "_uid", "is_converted"]
+utm_source_columns = list(data.filter(regex="utm_source_"))
+utm_campaign_columns = list(data.filter(regex="utm_campaign_"))
+non_journey_columns.extend(utm_campaign_columns)
+data["jvector"] = data.drop(columns=non_journey_columns).apply(list, axis=1)
+print(f"There are {len(data['jvector'].value_counts())} unique user journeys in this dataset")
