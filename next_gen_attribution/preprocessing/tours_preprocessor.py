d47627cc427db92c2b3d488c1926accac5731802
Hans Bantilan <hansbantilan@gmail.com>
2022-11-18 10:04:57 +0000

    add subset-value dictionary

--- next_gen_attribution/preprocessing/tours_preprocessor.py
+++ next_gen_attribution/preprocessing/tours_preprocessor.py
@@ -9,16 +9,16 @@ data["utm_source_email"] = data["utm_source_email"] + data["utm_source_adobe"]
 data = data.drop(columns=["utm_source_adobe"])
 
 # define column subsets (TODO: refactor)
-non_journey_columns = ["user_id", "_uid", "is_converted"]
+non_touchpoints = ["user_id", "_uid", "is_converted"]
 utm_source_columns = list(data.filter(regex="utm_source_"))
 utm_campaign_columns = list(data.filter(regex="utm_campaign_"))
-non_journey_columns.extend(utm_campaign_columns)
-journey_columns = utm_source_columns
+non_touchpoints.extend(utm_campaign_columns)
+touchpoints = utm_source_columns
 
 # disregard repeated occurences of the same touchpoint by clipping the number of occurences to a max of 1
-for journey_column in journey_columns:
+for journey_column in touchpoints:
     data[journey_column] = data[journey_column].clip(0,1) 
 
 # create journey vector
-data["jvector"] = data.drop(columns=non_journey_columns).apply(tuple, axis=1)
+data["jvector"] = data.drop(columns=non_touchpoints).apply(tuple, axis=1)
 print(f"There are {len(data['jvector'].value_counts())} unique user journeys in this dataset")
